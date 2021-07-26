3e9e583f1b1bf95330b9fe3070a609f699ae414b
Abiodun Awoyemi <aabiodunawoyemi@gmail.com>
2021-07-26 22:39:26 +0100

    scroll to top effect added

--- script.js
+++ script.js
@@ -17,3 +17,25 @@ typewriter.typeString('I am a frontend Software Developer.')
     .typeString('I am a content creator')
     .pauseFor(1500)
     .start();
+
+
+
+//Get the button:
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = () => scrollFunction();
+
+const scrollFunction = () => {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+const topFunction = () => {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
